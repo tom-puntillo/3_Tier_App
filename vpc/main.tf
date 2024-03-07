4497340be8d612ffe03429aa30483e14e5cdfd20
tom-puntillo <tom.puntillo@gmail.com>
2024-03-07 22:26:17 +0000

    added the subnet resource to the vpc module

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,9 +1,43 @@
 #---vpc-main.tf
 
-resource "aws_vpc" "_3_tier_vpc" {
+resource "aws_vpc" "three_tier_vpc" {
     cidr_block = "10.0.0.0/16"
     
     tags = {
         Name = "3_tier_vpc"
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+ vpc_id = aws_vpc.three_tier_vpc.id
+ cidr_block = var.public_subnet
+ availability_zone = "us-east-1a"
+ map_public_ip_on_launch = true
+ 
+ tags = {
+     Name = "web_subnet"
+ }
+ 
+}
+
+resource "aws_subnet" "private_subnet_1" {
+    vpc_id = aws_vpc.three_tier_vpc.id
+    cidr_block = var.private_subnet_1
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = true
+    
+    tags = {
+        Name = "logic_subnet"
+    }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+    vpc_id = aws_vpc.three_tier_vpc.id
+    cidr_block = var.private_subnet_2
+    availability_zone = "us-east-1c"
+    map_public_ip_on_launch = true
+    
+    tags = {
+        Name = "database_subnet"
     }
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,1 +1,19 @@
 #---vpc-variables.tf
+
+variable "public_subnet" {
+    
+    type = string
+    default = "10.0.1.0/24"
+}
+
+variable "private_subnet_1" {
+    
+    type = string
+    default = "10.0.2.0/24"
+}
+
+variable "private_subnet_2" {
+    
+    type = string
+    default = "10.0.3.0/24"
+    }
