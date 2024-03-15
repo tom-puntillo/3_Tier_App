bd0470d7164e326e746cc76e099df513570e04c0
tom-puntillo <tom.puntillo@gmail.com>
2024-03-15 21:58:54 +0000

    added comments to ec2 variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,15 +1,18 @@
 #---ec2-variables.tf
 
+# Define a variable named "ami" with type string and a default value of "ami-0005e0cfe09cc9050".
 variable "ami" {
   type    = string
   default = "ami-0005e0cfe09cc9050"
 }
 
+# Define a variable named "instance_type" with type string and a default value of "t2.micro".
 variable "instance_type" {
   type    = string
   default = "t2.micro"
 }
 
+# Define a variable named "user_data" with type string and a default value containing encoded shell script for initializing the instance.
 variable "user_data" {
   type    = string
   default = "IyEvYmluL2Jhc2gKCnN1ZG8geXVtIHVwZGF0ZSAteQpzdWRvIHl1bSB1cGdyYWRlIC15CnN1ZG8geXVtIGluc3RhbGwgbmdpbnggLXkKc3VkbyBzeXN0ZW1jdGwgc3RhcnQgbmdpbngKc3VkbyBzeXN0ZW1jdGwgZW5hYmxlIG5naW54Cg=="
