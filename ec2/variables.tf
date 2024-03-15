f2ddac735c3c61643460b45c2491c339c6e1c3cf
tom-puntillo <tom.puntillo@gmail.com>
2024-03-15 17:53:14 +0000

    added some variables to the ec2 module

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,5 +1,20 @@
 #---ec2-variables.tf
 
+variable "ami" {
+  type    = string
+  default = "ami-0005e0cfe09cc9050"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "user_data" {
+  type    = string
+  default = "IyEvYmluL2Jhc2gKCnN1ZG8geXVtIHVwZGF0ZSAteQpzdWRvIHl1bSB1cGdyYWRlIC15CnN1ZG8geXVtIGluc3RhbGwgbmdpbnggLXkKc3VkbyBzeXN0ZW1jdGwgc3RhcnQgbmdpbngKc3VkbyBzeXN0ZW1jdGwgZW5hYmxlIG5naW54Cg=="
+}
+
 # Define a variable named "security_groups" which expects a list of strings.
 variable "security_groups" {
   type        = list(string)              # Specify that the variable should be a list of strings.
