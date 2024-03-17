93a0912b27e6f562fb38ff80219c10afbf79ded8
tom-puntillo <tom.puntillo@gmail.com>
2024-03-17 23:51:19 +0000

    created an autoscaling group and corrected the code for the launch template

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -9,41 +9,37 @@ output "vpc_id" {
 }
 
 # Output the ID of public subnet 1
-output "public_subnet_1" {
+output "public_subnet_1_cidr" {
   description = "public subnet 1" # Description of the output
-  value = {
-    id = aws_subnet.public_subnet_1.id
-  }
+  value = aws_subnet.public_subnet_1.cidr_block
 }
 
 # Output the ID of public subnet 2
-output "public_subnet_2" {
+output "public_subnet_2_cidr" {
   description = "public subnet 2" # Description of the output
-  value = {
-    id = aws_subnet.public_subnet_2.id
-  }
+  value = aws_subnet.public_subnet_2.cidr_block
 }
 
 # Output the ID of private subnet 1
-output "private_subnet_1" {
+output "private_subnet_1_cidr" {
   description = "private subnet 1"             # Description of the output
-  value       = aws_subnet.private_subnet_1.id # Value to output (ID of the subnet)
+  value       = aws_subnet.private_subnet_1.cidr_block # Value to output (ID of the subnet)
 }
 
 # Output the ID of private subnet 2
-output "private_subnet_2" {
+output "private_subnet_2_cidr" {
   description = "private subnet 2"             # Description of the output
-  value       = aws_subnet.private_subnet_2.id # Value to output (ID of the subnet)
+  value       = aws_subnet.private_subnet_2.cidr_block # Value to output (ID of the subnet)
 }
 
 # Output the ID of private subnet 3
-output "private_subnet_3" {
+output "private_subnet_3_cidr" {
   description = "private subnet 3"             # Description of the output
-  value       = aws_subnet.private_subnet_3.id # Value to output (ID of the subnet)
+  value       = aws_subnet.private_subnet_3.cidr_block # Value to output (ID of the subnet)
 }
 
 # Output the ID of private subnet 4
-output "private_subnet_4" {
+output "private_subnet_4_cidr" {
   description = "private subnet 4"             # Description of the output
-  value       = aws_subnet.private_subnet_4.id # Value to output (ID of the subnet)
+  value       = aws_subnet.private_subnet_4.cidr_block # Value to output (ID of the subnet)
 }
