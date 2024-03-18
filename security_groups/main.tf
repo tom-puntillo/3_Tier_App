e29165756901ce62bb6f028906961681abaceae1
tom-puntillo <tom.puntillo@gmail.com>
2024-03-18 14:56:49 +0000

    added security group to security groups module main.tf and outputs.tf

--- security_groups/main.tf
+++ security_groups/main.tf
@@ -29,3 +29,30 @@ resource "aws_security_group" "allow_http" {
     Name = "allow_http" # Set the tag 'Name' to "allow_http"
   }
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "HTTPS from Anywhere"
+  description = "Allows HTTPS inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    to_port     = 443
+    from_port   = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    to_port          = 0
+    from_port        = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow tls"
+  }
+}
+
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -11,35 +11,35 @@ output "vpc_id" {
 # Output the ID of public subnet 1
 output "public_subnet_1_cidr" {
   description = "public subnet 1" # Description of the output
-  value = aws_subnet.public_subnet_1.cidr_block
+  value       = aws_subnet.public_subnet_1.cidr_block
 }
 
 # Output the ID of public subnet 2
 output "public_subnet_2_cidr" {
   description = "public subnet 2" # Description of the output
-  value = aws_subnet.public_subnet_2.cidr_block
+  value       = aws_subnet.public_subnet_2.cidr_block
 }
 
 # Output the ID of private subnet 1
 output "private_subnet_1_cidr" {
-  description = "private subnet 1"             # Description of the output
+  description = "private subnet 1"                     # Description of the output
   value       = aws_subnet.private_subnet_1.cidr_block # Value to output (ID of the subnet)
 }
 
 # Output the ID of private subnet 2
 output "private_subnet_2_cidr" {
-  description = "private subnet 2"             # Description of the output
+  description = "private subnet 2"                     # Description of the output
   value       = aws_subnet.private_subnet_2.cidr_block # Value to output (ID of the subnet)
 }
 
 # Output the ID of private subnet 3
 output "private_subnet_3_cidr" {
-  description = "private subnet 3"             # Description of the output
+  description = "private subnet 3"                     # Description of the output
   value       = aws_subnet.private_subnet_3.cidr_block # Value to output (ID of the subnet)
 }
 
 # Output the ID of private subnet 4
 output "private_subnet_4_cidr" {
-  description = "private subnet 4"             # Description of the output
+  description = "private subnet 4"                     # Description of the output
   value       = aws_subnet.private_subnet_4.cidr_block # Value to output (ID of the subnet)
 }
