f29797b27728fdf9b3c0da9b4112c959fa6e7646
tom-puntillo <tom.puntillo@gmail.com>
2024-04-01 15:03:52 +0000

    added allow_mysql security group

--- security_groups/main.tf
+++ security_groups/main.tf
@@ -109,3 +109,31 @@ resource "aws_security_group_rule" "allow_ssh_logic" {
   security_group_id        = aws_security_group.allow_http_and_tls_logic.id # Allow from the defined security group
 }
 
+resource "aws_security_group" "allow_mysql" {
+  name        = "db_security_group"            # Set the name of the security group to "web_security_group"
+  description = "Allows MySQL inbound traffic" # Provide a description for the security group
+  vpc_id      = var.vpc_id                     # Set the VPC ID for the security group based on the input variable 'vpc_id'
+
+  # Define inbound traffic rules for HTTP
+  ingress {
+    description     = "MySQL from Logic Tier"                          # Set the description for the ingress rule
+    to_port         = 3306                                             # Allow traffic to port 80
+    from_port       = 3306                                             # Allow traffic from port 80
+    protocol        = "tcp"                                            # Allow TCP protocol
+    security_groups = [aws_security_group.allow_http_and_tls_logic.id] # Allow traffic from any IPv4 address
+  }
+
+  # Define outbound traffic rules
+  egress {
+    from_port        = 0             # Allow traffic from any port
+    to_port          = 0             # Allow traffic to any port
+    protocol         = "-1"          # Allow all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # Allow traffic to any IPv4 address
+    ipv6_cidr_blocks = ["::/0"]      # Allow traffic to any IPv6 address
+  }
+
+  # Set tags for the security group
+  tags = {
+    Name = "allow_mysql_traffic_from_logic_tier" # Set the tag 'Name' to "allow_traffic_from_internet"
+  }
+}
