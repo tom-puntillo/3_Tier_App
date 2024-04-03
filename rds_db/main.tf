52395ab16be37dbcfe2be4503a063039be94632c
tom-puntillo <tom.puntillo@gmail.com>
2024-04-03 14:34:41 +0000

    created the rds_db module

---
+++ rds_db/main.tf
@@ -0,0 +1,27 @@
+#---rds-main.tf
+
+resource "aws_db_subnet_group" "aurora_subnet_group" {
+  name       = "aurora_subnet_group"
+  subnet_ids = [var.private_subnet_3_id, var.private_subnet_4_id]
+}
+
+resource "aws_rds_cluster" "aurora_cluster" {
+  cluster_identifier      = "aurora-cluster"
+  engine                  = var.engine
+  engine_version          = var.engine_version
+  database_name           = var.db_name
+  master_username         = var.master_username
+  master_password         = var.master_password
+  backup_retention_period = 30
+  preferred_backup_window = "06:00-08:00"
+  skip_final_snapshot     = false
+  db_subnet_group_name    = aws_db_subnet_group.aurora_subnet_group.name
+}
+
+resource "aws_rds_cluster_instance" "aurora_instance" {
+  cluster_identifier = aws_rds_cluster.aurora_cluster.id
+  instance_class     = var.db_instance
+  engine             = var.engine
+  engine_version     = var.engine_version
+  identifier         = "aurora-instance-0"
+}
